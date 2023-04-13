34a97891b85868f44a35d3a30fd1bbf7091412cc
Artur Assis Alves <arturassiscomp@gmail.com>
2023-04-13 20:37:55 -0300

    Extract constant from input field

--- lib/widgets/input_field.dart
+++ lib/widgets/input_field.dart
@@ -1,3 +1,4 @@
+import 'package:flash_chat/constants.dart';
 import 'package:flutter/material.dart';
 
 class InputField extends StatelessWidget {
@@ -13,21 +14,8 @@ class InputField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextField(
       onChanged: onChanged,
-      decoration: InputDecoration(
+      decoration: kDefaultInputFieldDecoration.copyWith(
         hintText: hintText,
-        contentPadding:
-            const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-        border: const OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(32.0)),
-        ),
-        enabledBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-          borderRadius: BorderRadius.all(Radius.circular(32.0)),
-        ),
-        focusedBorder: const OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-          borderRadius: BorderRadius.all(Radius.circular(32.0)),
-        ),
       ),
     );
   }
