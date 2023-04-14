dcb3a38722347baae0d546ae965d29690ce5a006
Artur Assis Alves <arturassiscomp@gmail.com>
2023-04-14 13:41:34 -0300

    Implement user registration.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flash_chat/constants.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/widgets/basic_button.dart';
 import 'package:flash_chat/widgets/email_input.dart';
 import 'package:flash_chat/widgets/hero_image_asset.dart';
 import 'package:flash_chat/widgets/password_input.dart';
 import 'package:flash_chat/widgets/space_between.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
@@ -16,6 +18,29 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final auth = FirebaseAuth.instance;
+  String? _email;
+  String? _password;
+
+  Future<bool> _registerUser() async {
+    if (_email != null && _password != null) {
+      try {
+        final userCredentials = await auth.createUserWithEmailAndPassword(
+            email: _email!, password: _password!);
+        if (userCredentials.user != null) {
+          return true;
+        }
+      } catch (e) {
+        print(e);
+      }
+    }
+    return false;
+  }
+
+  void _gotoChatScreen() {
+    Navigator.pushNamed(context, ChatScreen.routeName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,17 +56,30 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
             const SpaceBetween(
               verticalSpace: 48,
             ),
-            EmailInput(),
+            EmailInput(
+              onChanged: (value) {
+                _email = value;
+              },
+            ),
             const SpaceBetween(
               verticalSpace: 8,
             ),
-            PasswordInput(),
+            PasswordInput(
+              onChanged: (value) {
+                _password = value;
+              },
+            ),
             const SpaceBetween(
               verticalSpace: 24,
             ),
             BasicButton(
               text: 'Register',
-              onPressed: () {},
+              onPressed: () async {
+                final bool userCreated = await _registerUser();
+                if (userCreated) {
+                  _gotoChatScreen();
+                }
+              },
               color: Colors.blueAccent,
             ),
           ],
